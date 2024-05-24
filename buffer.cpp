9719c716f6575a13e582ed86840c1eded664e3af
Juan Esteban Paez Alfonso <159261171+Paez1542@users.noreply.github.com>
2024-05-23 23:48:39 -0500

    Update buffer.cpp

--- buffer.cpp
+++ buffer.cpp
@@ -1,6 +1,7 @@
 /**************************************
-Autores:  Carlos Mejia
-          Juan Paez
+Autores:  Juan Paez
+          Carlos Mejia
+          
 
 Fecha: 28/04/2024
 Tema: PROYECTO (Monitoreo de Sensores)
